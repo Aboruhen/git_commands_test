686369823fb246b5069435df44c9a5fb0d7b230b
Roman Kniazevych <aboruhen@gmail.com>
2025-05-13 12:37:20 +0300

    Add security features

--- core-module/src/main/java/com/example/git/model/DataRecord.java
+++ core-module/src/main/java/com/example/git/model/DataRecord.java
@@ -10,4 +10,4 @@ public class DataRecord {
     private String content;
     private long timestamp;
     private String metadata;
-} 
+} // TODO: Add security features - Branch: feature/security-enhancement
